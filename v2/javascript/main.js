9e3e77b7ef6a77fb84aa1706efe02fe826191aa5
Kartikeya Parihar <kartikeyaparihar@Kartikeyas-MacBook-Pro.local>
2022-01-02 00:56:38 -0800

    Redo

---
+++ v2/javascript/main.js
@@ -0,0 +1,33 @@
+function addEvents() {
+  meLink = document.getElementById("me");
+  meSection = document.querySelector(".main");
+  scrollToEl(meLink, meSection);
+
+  projectsLink = document.getElementById("projects");
+  projectsSection = document.querySelector(".projects");
+  scrollToEl(projectsLink, projectsSection);
+
+  skillsLink = document.getElementById("skills");
+  skillsSection = document.querySelector(".skills");
+  scrollToEl(skillsLink, skillsSection);
+}
+
+function scrollToEl(link, el) {
+  link.addEventListener("mouseover", () => {
+    console.log("gets here", el.offsetTop + 'px');
+    window.scrollTo({
+      top: el.offsetTop,
+      behavior: "smooth",
+    })
+  }, false);
+  link.addEventListener("click", () => {
+    console.log("gets here", el.offsetTop + 'px');
+    window.scrollTo({
+      top: el.offsetTop,
+      behavior: "smooth",
+    })
+  }, false);
+}
+
+document.addEventListener('DOMContentLoaded', addEvents, false);
+
