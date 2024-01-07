d4046e135f7355eb30e4b5afef642d4a4a8d9121
Kartikeya Parihar <kartikvips@gmail.com>
2024-01-07 13:43:40 -0800

    add ga

--- v2/javascript/main.js
+++ v2/javascript/main.js
@@ -7,6 +7,10 @@ function addEvents() {
   projectsSection = document.querySelector(".projects");
   scrollToEl(projectsLink, projectsSection);
 
+  // workLink = document.getElementById("work");
+  // workSection = document.querySelector(".work");
+  // scrollToEl(workLink, workSection);
+
   skillsLink = document.getElementById("skills");
   skillsSection = document.querySelector(".skills");
   scrollToEl(skillsLink, skillsSection);
@@ -14,7 +18,6 @@ function addEvents() {
 
 function scrollToEl(link, el) {
   link.addEventListener("click", () => {
-    console.log("gets here", el.offsetTop + 'px');
     window.scrollTo({
       top: el.offsetTop,
       behavior: "smooth",
